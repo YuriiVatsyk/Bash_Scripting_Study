b3fecd2ffad0eb7251ad93d1399fb829190e2028
Yurii Vatsyk <yuriivatsyk@IFLT-00012.lan>
2021-11-10 15:51:49 +0200

    Task 4 v. 2

--- task4.sh
+++ task4.sh
@@ -14,3 +14,36 @@ if [ -e "$FILE" ]; then
 		echo "$FILE is readable"
 	fi
 fi
+
+#Strings
+
+STRING=" "
+
+if [ -z "$STRING" ]; then 
+	echo "There is no string." >&2
+	exit 1
+fi
+
+#Test integer expressions and double parentheses
+
+
+a=3
+b=4
+c=3
+if (( "$a" < "$b" )); then
+	echo "$a is less than $b."
+else
+	echo "$a is not less than $b."
+fi
+if (( "$a" != "$c" )); then
+	echo "$a is not equal to $c."
+else
+	echo "$a is equal to $c."
+fi
+#arithmetic expansion
+echo "$a + $b = $(( $a + $b ))"
+
+#Output
+# 3 is less than 4.
+# 3 is equal to 3.
+# 3 + 4 = 7
